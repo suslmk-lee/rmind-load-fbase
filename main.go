8eabfc6e7fe217657863914595968012615f6de4
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-08-16 13:09:25 +0900

    update

--- main.go
+++ main.go
@@ -119,9 +119,13 @@ func processIssueData(client *firestore.Client, ctx context.Context, cloudEvent 
 		return err
 	}
 
-	data.Notes = common.ReplaceOrRemove(data.Notes, replacements)
-	data.Subject = common.ReplaceOrRemove(data.Subject, replacements)
-	data.Description = common.ReplaceOrRemove(data.Description, replacements)
+	err, data = changeTimeZone(data)
+	if err != nil {
+		log.Printf("Failed to change data: %v", err)
+		return err
+	}
+
+	data = changeString(data)
 
 	firestoreData := map[string]interface{}{
 		"specversion": cloudEvent.SpecVersion,
@@ -134,6 +138,36 @@ func processIssueData(client *firestore.Client, ctx context.Context, cloudEvent 
 	}
 	_, err = client.Collection("issues").Doc(cloudEvent.ID).Set(ctx, firestoreData)
 	return err
+}
+
+func changeTimeZone(data model.IssueData) (error, model.IssueData) {
+	seoulLocation, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		return err, data
+	}
+
+	data.DueDate = time.Date(
+		data.DueDate.Year(), data.DueDate.Month(), data.DueDate.Day(),
+		data.DueDate.Hour(), data.DueDate.Minute(), data.DueDate.Second(), data.DueDate.Nanosecond(), seoulLocation).UTC()
+	data.CreatedOn = time.Date(
+		data.CreatedOn.Year(), data.CreatedOn.Month(), data.CreatedOn.Day(),
+		data.CreatedOn.Hour(), data.CreatedOn.Minute(), data.CreatedOn.Second(), data.CreatedOn.Nanosecond(), seoulLocation).UTC()
+	data.UpdatedOn = time.Date(
+		data.UpdatedOn.Year(), data.UpdatedOn.Month(), data.UpdatedOn.Day(),
+		data.UpdatedOn.Hour(), data.UpdatedOn.Minute(), data.UpdatedOn.Second(), data.UpdatedOn.Nanosecond(), seoulLocation).UTC()
+	data.StartDate = time.Date(
+		data.StartDate.Year(), data.StartDate.Month(), data.StartDate.Day(),
+		data.StartDate.Hour(), data.StartDate.Minute(), data.StartDate.Second(), data.StartDate.Nanosecond(), seoulLocation).UTC()
+
+	return nil, data
+}
+
+func changeString(data model.IssueData) model.IssueData {
+	data.Notes = common.ReplaceOrRemove(data.Notes, replacements)
+	data.Subject = common.ReplaceOrRemove(data.Subject, replacements)
+	data.Description = common.ReplaceOrRemove(data.Description, replacements)
+
+	return data
 }
 
 func processObjectKey(sess *session.Session, client *firestore.Client, ctx context.Context, prefix string, key string) {
